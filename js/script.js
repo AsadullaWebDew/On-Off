e1164f572f2bdf9a4189a8dee262f32e884d354d
AsadullaWebDew <asadullaxamiullayev@gmail.com>
2023-02-02 22:35:16 +0500

    On  Off

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let body = document.getElementById("body");
+let card = document.getElementById( "card");
+let rasm =document.getElementById("chiroq")
+
+function switchOn() {
+    rasm.src = "./images/on.jpg"
+}
+function switchOff() {
+    rasm.src = "images/off.jpg"
+}
+
